5256146934c551ce724489cab17c7c9da63c8b5d
Bruno Kamada <brunokamada72@gmail.com>
2022-08-09 15:28:31 -0300

    refactor: move duplicate code to a helper file

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -41,11 +41,6 @@ const simulateValidSubmit = async (
   await waitFor(() => form)
 }
 
-const testElementExists = (sut: RenderResult, fieldName: string): void => {
-  const el = sut.getByTestId(fieldName)
-  expect(el).toBeTruthy()
-}
-
 describe('Signup Component', () => {
   afterEach(cleanup)
 
@@ -124,6 +119,6 @@ describe('Signup Component', () => {
   test('Should show spinner on submit', async () => {
     const { sut } = makeSut()
     await simulateValidSubmit(sut)
-    testElementExists(sut, 'spinner')
+    Helper.testElementExists(sut, 'spinner')
   })
 })
